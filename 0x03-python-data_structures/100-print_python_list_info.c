8837657394354d012c0cb53ff5920fcfb29e772f
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-09-13 07:22:58 +0200

    Function that prints some basic info about Python lists.

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,24 +1,15 @@
-#include <object.h>
 #include <Python.h>
+#include <object.h>
 #include <listobject.h>
 
-/**
- * print_python_list_info - prints some basic info
- * about Python list
- * @p: pointer of type PyObectj.
- * Return: nothing.
-*/
 void print_python_list_info(PyObject *p)
 {
-	int i = 0;
-	PyListObject *t;
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
 
-	t = (PyListObject *)p;
-	printf("[*] Size of the Python List = %ld\n", t->ob_base.ob_size);
-	printf("[*] Allocated  = %ld\n", t->allocated);
-	while (i < t->ob_base.ob_size)
-	{
-		printf("Element %d: %s\n", i, t->ob_item[i]->ob_type->tp_name);
-		i++;
-	}
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
---
+++ 0x03-python-data_structures/t.c
@@ -0,0 +1,23 @@
+#include "Python.h"
+#include <stdio.h>
+
+/**
+ * print_python_list_info - prints some basic info
+ * about Python list
+ * @p: pointer of type PyObectj.
+ * Return: nothing.
+*/
+void print_python_list_info(PyObject *p)
+{
+	int i = 0;
+	PyListObject *t;
+
+	t = (PyListObject *)p;
+	printf("[*] Size of the Python List = %ld\n", t->ob_base.ob_size);
+	printf("[*] Allocated  = %ld\n", t->allocated);
+	while (i < t->ob_base.ob_size)
+	{
+		printf("Element %d: %s\n", i, t->ob_item[i]->ob_type->tp_name);
+		i++;
+	}
+}
