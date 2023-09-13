c58969376fac396651bbfbe1e15c3e0fad146087
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-09-13 19:26:32 +0200

    function that replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is not None and isinstance(a_dictionary, dict):
-        sorted_dict = dict(sorted(a_dictionary.items()))
-        for key, value in sorted_dict.items():
-            print("{:s}: {}".format(key, value))
-    else:
-        print("Invalid dictionary or None")
+    sorted_dict = dict(sorted(a_dictionary.items()))
+    for key, value in sorted_dict.items():
+        print("{:s}: {}".format(key, value))
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for key, value in a_dictionary.items():
-        if key:
-            a_dictionary[key] = value
-        else:
-            a_dictionary[key] = value
-            print("{:s}: {}".format(key, value))
+    a_dictionary[key] = value
+    return a_dictionary
