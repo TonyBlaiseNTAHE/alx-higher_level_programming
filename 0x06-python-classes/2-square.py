0a31f2a5b799a7a5aa28975580ce7d4fe4575be1
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-09-28 19:26:04 +0200

    creating class Square and its propriety

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Representing a class square
+    This a class representation and its properties"""
+
+
+class Square:
+    """Defining __init__ function"""
+    def __init__(self, size=0):
+        """check if size is an integer, if yes and instantiate it"""
+        if isintance(size, int):
+            """initializes __size of self with size"""
+            self.__size = size
+        else:
+            """ raise an TypeError message """
+            raise TypeError("size must be an intger")
+        if size < 0:
+            """raise an ValueError message """
+            raise ValueError("size must be >= 0")
