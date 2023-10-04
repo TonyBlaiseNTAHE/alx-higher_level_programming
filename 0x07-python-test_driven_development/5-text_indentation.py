769fb794514b381904095a6d1771e5b03c08551f
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-10-04 19:41:59 +0200

    function that print texts

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """
-function that prints a text with 2 new lines
-args: text - the text to print
+This is the "5-test_indentation" module.
+The 5-text_indentation module supplies one function, text_indentation(text).
 """
 
 
 def text_indentation(text):
-    """if statement"""
+    """splits a text into lines along "?", ":", "." followed by 2 new lines"""
     if type(text) is not str:
         raise TypeError("text must be a string")
     flag = 0
-    for i in text:
+    for a in text:
         if flag == 0:
-            if i == " ":
+            if a == ' ':
                 continue
             else:
                 flag = 1
         if flag == 1:
-            if i == '.' or i == '?' or i == ':':
-                print("{:s}".format(i))
+            if a == '?' or a == '.' or a == ':':
+                print(a)
                 print()
                 flag = 0
             else:
-                print("{:s}".format(i), end="")
+                print(a, end="")
