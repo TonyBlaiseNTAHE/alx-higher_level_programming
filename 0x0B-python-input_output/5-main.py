aa68ecf5964e9d09f415ce0ed2dff405478dbc22
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-10-11 18:53:50 +0200

    a script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/5-main.py
+++ 0x0B-python-input_output/5-main.py
@@ -2,7 +2,7 @@
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "my_list.json"
-my_list = [1, 2, 3]
+my_list = []
 save_to_json_file(my_list, filename)
 
 filename = "my_dict.json"
