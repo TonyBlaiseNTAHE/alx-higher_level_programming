d93bdcfd1d4cb94c50850150243be33f9137929f
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-11-18 18:10:35 +0200

    script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,2 @@
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_shows.title 'name' FROM tv_show_genres JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = 'Comedy' GROUP BY tv_shows.title ORDER BY tv_shows.title
+SELECT tv_shows.title 'title' FROM tv_show_genres JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = 'Comedy' GROUP BY tv_shows.title ORDER BY tv_shows.title
