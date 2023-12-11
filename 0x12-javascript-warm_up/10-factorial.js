4996218f64baa2ed5204c686c6ac5f28b1d08523
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-12-11 20:17:42 +0200

    a script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const i = parseInt(process.argv[2]);
+
+function fact (n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * fact(n - 1);
+  }
+}
+
+const result = fact(i);
+console.log(result);
