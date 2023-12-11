a7c7ad369de0340216c6d84789de4ab25fb98864
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-12-11 16:21:35 +0200

    a script that prints two arguments passed to it, in the following format: “ is ”

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const is = ' is ';
+const args = process.argv;
+let i = 2;
+
+if (process.argv[i] === undefined) {
+  console.log(process.argv[i] + is + process.argv[i]);
+}
+for (i = 2; i < args.length - 1; i++) {
+  console.log(process.argv[i] + is + process.argv[i + 1]);
+}
