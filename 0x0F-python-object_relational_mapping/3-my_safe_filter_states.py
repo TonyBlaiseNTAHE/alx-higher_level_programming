5ed88dcf4cc2102578f18f483c99154d6a1c8451
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-12-18 16:19:09 +0200

    a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
         db=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name=%s ORDER BY id ASC""",(sys.argv[4],))
+    cur.execute("""SELECT * FROM states WHERE name=%s
+                ORDER BY id ASC""", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
