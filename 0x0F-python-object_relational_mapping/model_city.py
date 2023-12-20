a7db59612a44b84c11b2bb2fe0e320a403b2ce58
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-12-20 18:07:22 +0200

    a Python file similar to model_state.py named model_city.py that contains the class definition of a City.

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,9 +9,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class City(Base):
     """city class"""
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True,
+                autoincrement=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
