43a74a59ef788f04f6609b510eef66ac5dcefe4d
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-02 08:14:44 +0200

    removing files

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-const map1 = require('./100-data.js').list;
-console.log(map1);
-console.log(map1.map((item, index) => item * index));
--- 0x13-javascript_objects_scopes_closures/5-square.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-class square extends require('./4-rectangle.js') {
-  constructor (size) {
-    super(size, size);
-  }
-}
-module.exports = square;
