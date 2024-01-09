11b189773ec579a61ec5a468a9c17d43efa52616
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-09 19:19:41 +0200

    a script that prints JavaScript is amazing

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const first_string = 'C is fun'
+const second_string = 'Python is cool'
+const third_string = 'JavaScript is amazing'
+console.log(first_string,'\n',second_string,'\n',third_string)
