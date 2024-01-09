bf326c02ac3a932e715f7100c719b406e99c9dfc
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-09 19:31:01 +0200

    a script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const first_string = 'C is fun'
-const second_string = 'Python is cool'
-const third_string = 'JavaScript is amazing'
-console.log(first_string,'\n',second_string,'\n',third_string)
+const firstString = 'C is fun';
+const secondString = 'Python is cool';
+const thirdString = 'JavaScript is amazing';
+console.log(firstString + '\n' + secondString + '\n' + thirdString);
