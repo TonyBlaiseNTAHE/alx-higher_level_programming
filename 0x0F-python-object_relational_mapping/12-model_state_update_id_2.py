4e6e2a842c9184b6955f3bcd2e35847f7d442fcc
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-14 18:42:29 +0200

    a script that changes the name of a State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+12-model_state_update_id_2 module
+"""
+
+import sqlalchemy
+from model_state import State, Base
+import sys
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    state_to_update = session.query(State).filter(State.id == 2).first()
+    if state_to_update:
+        state_to_update.name = 'New mexico'
+    session.commit()
+    session.close()
