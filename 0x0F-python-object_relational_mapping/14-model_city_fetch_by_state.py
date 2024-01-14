583905295d262044ed45f38daea98743dd87e48e
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-14 22:52:57 +0200

    a Python file similar to model_state.py named model_city.py that contains the class definition of a City.

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""
-9-model_state_filter_a module
-"""
-import sys
-from model_state import Base, State
-from model_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 
-if __name__ == "__main__":
+"""
+14-model_city_fetch_by_state module
+"""
+
+import sqlalchemy
+from model_state import State, Base
+from model_city import City
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
+                           .format(sys.argv[1],
+                                   sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     session = Session(engine)
-    for state, city in session.query(State, City)\
-            .filter(City.state_id == State.id).order_by(City.id).all():
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    for state, city in \
+        session.query(State, City)\
+            .join(City, State.id == City.state_id).order_by(City.id).all():
+        print('{}: ({}) {}'.format(state.name, city.id, city.name))
     session.close()
