3ede9a69c93c89fcc4c8d4905a3ae2f1f1b99536
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-20 21:19:49 +0200

    a script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -16,7 +16,5 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state')
-
-    def __init__(self, name):
-        self.name = name
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
