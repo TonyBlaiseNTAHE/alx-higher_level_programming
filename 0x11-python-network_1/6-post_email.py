3658a8fb57fa3d468d739219b4f4592057d274f3
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-30 19:36:13 +0200

    Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
 """
-6-post_email module
-"""
-
-from requests import post
-from sys import argv
 
 
-if __name__ == '__main__':
-    content = post(argv[1], data={'email': argv[2]})
-    print(content.text)
+if __name__ == "__main__":
+    from requests import post
+    from sys import argv
+
+    html = post(argv[1], data={'email': argv[2]})
+    print(html.text)
