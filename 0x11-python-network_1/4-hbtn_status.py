efe4f917595ea573675fd42f8163cc0456b07c9a
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-30 18:43:42 +0200

    fixing errors

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
 """
-4-hbtn_status module
-"""
-
-from requests import get
 
 
-if __name__ == '__main__':
-        content = get('https://alx-intranet.hbtn.io/status')
-        print('Body response:')
-        print('\t- type: {}'.format(type(content.text.__class__)))
-        print('\t- content: {}'.format(content.text))
+if __name__ == "__main__":
+    from requests import get
+
+    html = get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
