72f05555543f89fa89e02238d58bcbbfbf36567d
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-31 10:43:12 +0200

    Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,7 @@ send a post request to an URL with a letter
 if __name__ == '__main__':
     import sys
     import requests
-    
+
     url = 'http://0.0.0.0:5000/search_user'
     if len(sys.argv) > 1:
         q = sys.argv[1]
