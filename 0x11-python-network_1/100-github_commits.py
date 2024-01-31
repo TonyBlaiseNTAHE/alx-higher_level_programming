e4f1ec1513e9c24d579ae53aa84abd99979319b4
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-31 11:24:59 +0200

    Time for interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,8 @@ from requests import get
 
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2], sys.argv[1])
+    url = 'https://api.github.com/repos/{}/{}/commits'\
+        .format(sys.argv[2], sys.argv[1])
     commits = get(url).json()
     try:
         for i in range(10):
