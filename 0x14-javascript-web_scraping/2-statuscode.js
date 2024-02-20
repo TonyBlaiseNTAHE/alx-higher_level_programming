7f827c87c83b991e2554743d4a01427b120cff10
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-02-20 17:57:25 +0200

    a script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-/**
- * script that sends a GET request
- */
 const request = require('request');
 request(process.argv[2], function (error, response) {
   if (error == null) {
-    console.log('code:' + response.statusCode);
+    console.log('code: ' + response.statusCode);
   }
 });
