d42b1f2ba0af0b8d039d8a0431f2c96c7fbf2349
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-02-20 17:02:56 +0200

    a script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * script that sends a GET request
+ */
+const axios = require('axios');
+
+const url = process.argv[2];
+
+axios.get(url)
+    .then(response => {
+        console.log('code:', response.status);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
