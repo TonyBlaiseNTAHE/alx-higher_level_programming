9ebcec9321c6d5c246adf2983157c8ab133570e2
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-02-20 17:49:42 +0200

    updates

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,10 +11,11 @@ if __name__ == '__main__':
     url = 'https://api.github.com/repos/{}/{}/commits'\
         .format(sys.argv[2], sys.argv[1])
     commits = get(url).json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    print(commits)
+    #try:
+        #for i in range(10):
+            #print("{}: {}".format(
+                #commits[i].get("sha"),
+                #commits[i].get("commit").get("author").get("name")))
+    #except IndexError:
+        #pass
